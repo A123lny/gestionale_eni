fb99164342c240c8a9196175c9a38ca4d0d1d2c3
A123lny <cervellini.91.andrea@gmail.com>
2026-04-27 18:31:09 +0200

    chore: clean up server.js file and improve code organization

--- print-server/server.js
+++ print-server/server.js
@@ -71,7 +71,7 @@ function saveLayout(layout) {
 }
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 // ============================================================
 // ESC/POS Commands
